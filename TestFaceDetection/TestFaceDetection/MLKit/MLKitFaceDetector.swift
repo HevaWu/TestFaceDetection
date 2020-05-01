9c06fa54e3be6679ae28c20be2c72fb8787eab07
Heva <he.wu@linecorp.com>
2020-05-01 17:18:12 +0900

    MLKitFaceDetector: update for showing MLKit rectangle

--- TestFaceDetection/TestFaceDetection/MLKit/MLKitFaceDetector.swift
+++ TestFaceDetection/TestFaceDetection/MLKit/MLKitFaceDetector.swift
@@ -7,13 +7,60 @@
 //
     
 import UIKit
+import FirebaseMLVision
 
-final class MLKitFaceDetector {
-    private let image: UIImage
-    private let parentView: UIView
+final class MLKitFaceDetector: FaceDetector {
+    internal let image: UIImage
+    internal let parentView: UIView
+    
+    private var reusableViews: [MLKitFaceOverlayView] {
+        if let existingViews = parentView.subviews as? [MLKitFaceOverlayView] {
+            return existingViews
+        } else {
+            return [MLKitFaceOverlayView]()
+        }
+    }
+    
+    private lazy var vision = Vision.vision()
+    private let options = VisionFaceDetectorOptions()
     
     init(image: UIImage, parentView: UIView) {
         self.image = image
         self.parentView = parentView
+    }
+    
+    func run() {
+        let faceDetector = vision.faceDetector(options: options)
+        let image = VisionImage(image: self.image)
+        faceDetector.process(image) { [weak self] res, error in
+            guard let self = self, error == nil, let faces = res, !faces.isEmpty else { return }
+            self.displayFaces(faces)
+        }
+    }
+    
+    func clearReusableViews() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            // Remove previously existing views that were not reused.
+            for view in self.reusableViews {
+                view.removeFromSuperview()
+            }
+        }
+    }
+    
+    private func displayFaces(_ faces: [VisionFace]) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            var reusableViews = self.reusableViews
+            for face in faces {
+                // Reuse existing observation view if there is one.
+                if let existingView = reusableViews.popLast() {
+                    existingView.face = face
+                } else {
+                    let newView = MLKitFaceOverlayView(face: face)
+                    self.parentView.addSubview(newView)
+                }
+            }
+        }
     }
 }
